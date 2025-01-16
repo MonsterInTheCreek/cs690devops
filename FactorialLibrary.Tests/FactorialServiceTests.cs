a442ec66d3ac2b41a15b7679859d38078e88ffd0
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2025-01-16 12:30:56 +0000

    Setup project and Tests

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,16 @@
+//namespace FactorialLibrary.Tests;
+namespace FactorialService.Tests;
+
+using FactorialLibrary;
+
+public class FactorialServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+        var result = FactorialService.Factorial(0);
+        Assert.Equal(1,result);
+
+    }
+}
